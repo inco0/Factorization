9b9eb7dd3605198ce3a65c8119741acacf11b5e3
Manos <kalathasmanos@gmail.com>
2023-01-29 22:47:29 +0200

    Update trial division to use mpz objects

--- factorization_algorithms/trial_division.pyx
+++ factorization_algorithms/trial_division.pyx
@@ -1,14 +1,15 @@
 #cython: language_level=3
 from math import sqrt
 from gmpy2 import mpz
-import cython
+from gmpy2 cimport *
 
-cpdef factorize(composite_number):
-    current_number = composite_number
-    cdef int prime = 2
-    divisors: [int] = []
 
-    while prime <= sqrt(composite_number):
+cpdef factorize(composite_number: int):
+    cdef mpz current_number = mpz(composite_number)
+    cdef mpz prime = 2
+    divisors: [mpz] = []
+
+    while prime <= mpz(sqrt(composite_number)):
         if current_number % prime == 0:
             divisors.append(prime)
             current_number //= prime
