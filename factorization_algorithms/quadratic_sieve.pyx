2bfa8d42858c3b2d8750c7ee06706e5dda337280
Manos <kalathas.manos@gmail.com>
2023-03-03 18:21:05 +0200

    Finish initialization part

--- factorization_algorithms/quadratic_sieve.pyx
+++ factorization_algorithms/quadratic_sieve.pyx
@@ -1,16 +1,18 @@
 #cython: language_level=3
 from gmpy2 cimport *
-from gmpy2 import mpz, is_prime, sqrt, ceil, exp, log, is_congruent, powmod, mpz_random
+from gmpy2 import mpz, is_prime, sqrt, exp, log, is_congruent, powmod, mpz_random, div
 from exceptions.exceptions import InvalidInput
-from math import pow
-from random import random
+from math import ceil
+from random import randint
 
 
 cpdef list get_quadratic_sieve_factorization(number_to_be_factored: int):
     factors: list[mpz] = []
     if number_to_be_factored % 2 == 0 or is_prime(number_to_be_factored):
         raise InvalidInput("Enter an odd composite number that is not a power")
+    print("1.PERFORMING INITIALIZATION")
     smooth_boundary, roots = initialization(number_to_be_factored)
+    print("2.PERFORMING SIEVING")
     # sieving()
     # linear_algebra()
     # factorize()
@@ -21,14 +23,16 @@ cpdef tuple initialization(number_to_be_factored):
     Initializes the variables needed to perform the quadratic sieve factorization
 
     :param number_to_be_factored: The number being factored
-    :return: A tuple with B and a list of roots such that root_i^2≡n(mod p_i)
+    :return: A tuple with smooth boundary B and a list of roots such that root_i^2≡n(mod p_i)
     """
     cdef mpz n = mpz(number_to_be_factored)
     cdef mpz smooth_boundary = mpz(
         ceil(sqrt(exp(sqrt(log(n) * log(log(n)))))))  # The value of B is √(e^(√(ln(n)ln(ln(n))))
+    print(f"Smooth boundary is {smooth_boundary}")
     quadratic_roots = get_quadratic_roots(number_to_be_factored, smooth_boundary)
+    return smooth_boundary, quadratic_roots
 
-cpdef legendre(num, prime):
+cpdef legendre(num, p):
     """
     Returns the value of the legendre symbol between n and prime based on the following cases:
     1 if n is a quadratic residue modulo prime and n≢0(mod prime)
@@ -36,14 +40,15 @@ cpdef legendre(num, prime):
     0 if n ≡ 0(mod prime)
     
     :param num: An integer
-    :param prime: An odd prime number
+    :param p: An odd prime number
     :return: The value of the legendre symbol between n and prime
     """
+    cdef prime = mpz(p)
     cdef mpz n = mpz(num % prime)
-    cdef mpz t = 1
+    cdef mpz t = mpz(1)
     while n != 0:
         while n % 2 == 0:
-            n /= 2
+            n //= 2
             if 3 <= (prime % 8) <= 5:
                 t = -t
         n, prime = prime, n
@@ -54,20 +59,20 @@ cpdef legendre(num, prime):
         return t
     return 0
 
-cpdef list sieve_of_eratosthenes(n):
+cpdef list sieve_of_eratosthenes(bound):
     """
-    Perform a sieving up to n and return a list of primes
+    Perform the sieve of eratosthenes up to n and return all the primes
 
-    :param n: The number up to which you sieve for primes
+    :param bound: The number up to which you sieve for primes
     :return: A list with the primes from 3 up to n 
     """
-    prime_flag = [True] * n
+    prime_flag = [True] * bound
     primes = []
-    for i in range(2, ceil(sqrt(n))):
+    for i in range(2, mpz(ceil(sqrt(bound)))):
         if prime_flag[i]:
-            for j in range(i * i, n, i):
+            for j in range(i * i, bound, i):
                 prime_flag[j] = False
-    return [prime for prime in range(n) if prime_flag[prime] == True and prime > 2]
+    return [prime for prime in range(bound) if prime_flag[prime] == True and prime > 2]
 
 cpdef list get_quadratic_roots(number_to_be_factored, smooth_bound):
     """
@@ -77,11 +82,13 @@ cpdef list get_quadratic_roots(number_to_be_factored, smooth_bound):
     :param smooth_bound: The B smooth bound
     :return: A list of roots 
     """
-    primes = sieve_of_eratosthenes(number_to_be_factored)
+    primes = sieve_of_eratosthenes(smooth_bound)
+    print("========PRIMES FOUND========")
     roots = []
     for prime in primes:
         if legendre(number_to_be_factored, prime) == 1:
-            pass
+            square_roots(number_to_be_factored, prime)
+    print("========SQUARE ROOTS FOUND========")
     return roots
 
 
@@ -100,13 +107,13 @@ cpdef square_roots(num, prime):
         return x, prime - x % prime
     elif is_congruent(prime, 5, 8):
         x = powmod(n, (prime + 3) // 8, prime)
-        c = (x*x) % prime
+        c = powmod(x, 2, prime)
         if not is_congruent(c, n, prime):
-            x = (x * powmod(2, (prime - 1) // 4), prime)
+            x = x * powmod(2, (prime - 1) // 4, prime)
         return x, prime - x % prime
     else : # is_congruent(prime, 1, 8)
         while True:
-            d = random.randint(2, prime - 1)
+            d = randint(2, prime - 1)
             if legendre(d, prime) == -1:
                 break
         s, t = represent_as_power_of_two(prime - 1)
@@ -114,7 +121,21 @@ cpdef square_roots(num, prime):
         d = powmod(d, t, prime)
         m = 0
         for i in range(s):
-            if is_congruent(pow(a*pow(d, m), pow(2, s-i-1)), -1, prime):
+            if is_congruent(int(pow(a*pow(d, m), pow(2, s-i-1))), -1, prime):
                 m += pow(2, i)
-        x = (pow(n, (t + 1) // 2) * pow(d, m // 2)) % prime
+        x = pow(mpz(n), mpz((t + 1) // 2)) * pow(mpz(d), mpz(m // 2)) % prime
         return x, prime - x % prime
+
+
+cpdef represent_as_power_of_two(n):
+    """
+    Represents n as 2^s*t with t odd
+    :param n: 
+    :return: 
+    """
+    cpdef int s = 1
+    cpdef int t = n // pow(2, s)
+    while t % 2 == 0:
+        s += 1
+        t = n // pow(2, s)
+    return s, t
--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-from factorization_algorithms import trial_division, fermat_square_difference, utils, quadratic_sieve
-from factorization_algorithms.pollard_rho import factorize, get_pollard_rho_factorization
 from gmpy2 import is_prime
 from exceptions.exceptions import InputIsPrimeNumber
+from factorization_algorithms.pollard_rho import get_pollard_rho_factorization
+from factorization_algorithms.quadratic_sieve import get_quadratic_sieve_factorization
 
 
-if __name__ == "__main__":
-    number_to_be_factored = 339126523890
-    if is_prime(number_to_be_factored):
-        raise InputIsPrimeNumber(f"{number_to_be_factored} is prime.")
-
-    factors = get_pollard_rho_factorization(number_to_be_factored)
+def pollard_rho(n: int):
+    factors = get_pollard_rho_factorization(n)
     result = 1
     for factor in factors:
         print(factor)
         result *= factor
-    assert (result == number_to_be_factored)
+    assert (result == n)
 
 
+def quadratic_sieve(n: int):
+    get_quadratic_sieve_factorization(n)
+
+
+if __name__ == "__main__":
+    number_to_be_factored = 800000987987698796780018000009879876987967800180000098798769879678001
+    if is_prime(number_to_be_factored):
+        raise InputIsPrimeNumber(f"{number_to_be_factored} is prime.")
+    # pollard_rho(number_to_be_factored)
+    print(f"Initializing quadratic sieve factoring of {number_to_be_factored}")
+    quadratic_sieve(number_to_be_factored)
