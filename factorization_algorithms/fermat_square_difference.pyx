efae8c649fa16b17d5d8206a13a4aa088d51dfdf
Manos <kalathas.manos@gmail.com>
2023-04-15 19:15:44 +0300

    Remove stuff

--- factorization_algorithms/fermat_square_difference.pyx
+++ factorization_algorithms/fermat_square_difference.pyx
@@ -10,7 +10,6 @@ cpdef list factorize_default(n: int):
     cdef mpz t = mpz(2*first_factor + 1)
     cdef mpz r_root = mpz(sqrt(abs(r)))
     while r_root*r_root != r:
-        print("ASD")
         r += t
         t += 2
         r_root = mpz(int(sqrt(abs(r))))
@@ -30,6 +29,3 @@ cpdef list factorize_hart(n: int, limit: int):
     cdef mpz t = mpz(sqrt(m))
     cdef factor = mpz(gcd(s-t, composite_number))
     return [factor, composite_number // factor]
-
-cpdef factorize_lehman(n):
-    pass
