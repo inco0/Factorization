f26342f4070d8aa16dbdab2411caab1879a03091
Manos <kalathas.manos@gmail.com>
2023-05-08 18:31:15 +0300

    Removed unused files

--- factorization_algorithms/dixon.pyx
+++
@@ -1,6 +0,0 @@
-#cython: language_level=3
-from gmpy2 cimport *
-from gmpy2 import mpz, mpz_random, random_state, is_prime, gcd
-
-cpdef list factorize(n: int):
-    cdef mpz composite_number = mpz(n)
