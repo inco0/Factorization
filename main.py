7f2d53a3b93a9e5fe90b1870da854c3ddebdd5f7
Manos <kalathas.manos@gmail.com>
2023-06-14 19:57:50 +0300

    Added reading input from user

--- main.py
+++ main.py
@@ -24,11 +24,28 @@ def setup_logger():
     logging.basicConfig()
     logger.setLevel(logging.DEBUG)
 
+def read_input() -> tuple:
+    print(f"Which algorithm would you like to use from the following: \n \
+    1. Trial division \n \
+    2. Fermat's square difference \n \
+    3. Dixon's factorization \n \
+    4. Pollard Rho's factorization \n \
+    5. Quadratic sieve \n ")
+    algorithm = int(input("Please enter the number corresponding to the algorithm: "))
+    number_to_be_factored = int(input("What number would you like to factor: "))
+    if is_prime(number_to_be_factored):
+        raise InputIsPrimeNumber()
 
+def perform_factorization(number_to_be_factored, algorithm):
+    pass
+    
 if __name__ == "__main__":
     setup_logger()
-    number_to_be_factored = 13274718411466765
-    if is_prime(number_to_be_factored):
-        raise InputIsPrimeNumber(f"{number_to_be_factored} is prime.")
-    # pollard_rho(number_to_be_factored)
-    quadratic_sieve(number_to_be_factored)
+    try:
+        number_to_be_factored, algorithm = read_input()
+        perform_factorization(number_to_be_factored, algorithm)
+    except InputIsPrimeNumber:
+        print("The number you entered is a prime.")
+    except ValueError:
+        print("Please enter a number.")
+    
